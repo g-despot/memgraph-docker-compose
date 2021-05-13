e95d33f1ed21b428f863b890e10659d1fa69044c
Ivan Despot <ivan.g.despot@outlook.com>
2021-05-13 10:58:22 +0200

    Initial commit

---
+++ memgraph/entrypoint/docker-entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mg_import_csv memgraph \
+    --nodes ./import-data/comment_nodes.csv 
+    --nodes ./import-data/forum_nodes.csv 
+    --relationships ./import-data/relationships.csv
+./memgraph
